32bbe55461e32e3dbddb024af57d8461c94df98e
mindangdang <157975270+mindangdang@users.noreply.github.com>
2026-05-02 08:53:33 +0000

    edited

--- project/backend/app/api/router.py
+++ project/backend/app/api/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from project.backend.app.api.routes import content, profile
+from project.backend.app.api.routes import auth
 
 
 api_router = APIRouter()
+api_router.include_router(auth.router, tags=["auth"])
 api_router.include_router(content.router, tags=["content"])
 api_router.include_router(profile.router, tags=["profile"])
--- project/frontend/src/main.tsx
+++ project/frontend/src/main.tsx
@@ -1,15 +1,14 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {StrictMode} from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const queryClient = new QueryClient();
+const GOOGLE_CLIENT_ID = "217086331535-n1na3k15bd6c6cllpj1am8hamjsl1ah9.apps.googleusercontent.com"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
-    </QueryClientProvider>
-  </StrictMode>,
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
